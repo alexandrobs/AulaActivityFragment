72c6d1a68b6275a8e1989118b0b1f5d3987fa85c
Alexandro B S <jornalsinal@gmail.com>
2025-04-07 22:17:45 -0300

    Aula sobre activities e fragments, app que alterna entre atividades e usa um botão para abrir uma activitie e um botão para fechar uma activitie

---
+++ app/src/main/java/com/example/aulaactivityfragment/DetalhesActivity.kt
@@ -0,0 +1,32 @@
+package com.example.aulaactivityfragment
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var buttonFechar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes)
+
+        buttonFechar = findViewById( R.id.btnFechar)
+
+        buttonFechar.setOnClickListener {
+            finish()
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
