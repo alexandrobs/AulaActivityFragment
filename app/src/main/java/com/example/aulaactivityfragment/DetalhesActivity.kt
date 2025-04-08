36b93ffcf9fb1622af69c6f688b58d54c5e2ecb9
Alexandro B S <jornalsinal@gmail.com>
2025-04-08 15:15:29 -0300

    Ciclo de vida de uma activity

--- app/src/main/java/com/example/aulaactivityfragment/DetalhesActivity.kt
+++ app/src/main/java/com/example/aulaactivityfragment/DetalhesActivity.kt
@@ -1,11 +1,14 @@
 package com.example.aulaactivityfragment
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+private const val TAG: String = "DetalhesActivity"
 
 class DetalhesActivity : AppCompatActivity() {
 
@@ -28,5 +31,37 @@ class DetalhesActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.i(TAG,"onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy")
+        super.onDestroy()
     }
 }
